8f9a6788cd9c8a8b37d63e47569656e8e078c191
Gayathri Girish <gayathri120906@gmail.com>
2025-08-17 16:03:43 +0530

    Add files via upload

---
+++ 02. Program_to_Input_Numbers_and_Print_Sum.cpp
@@ -0,0 +1,33 @@
+// Program to input two numbers and Print their Sum .
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+    int a , b , sum;
+
+    cout << "Enter the First Number = ";
+    cin >> a;
+
+    cout << "Enter the Second Number = ";
+    cin >> b;
+
+    sum = a + b;
+    cout << "The Sum of " << a << " and " << b << " is = " << sum << endl;
+
+    return 0;
+}
+
+
+
+/*
+
+Output -
+
+Enter the First Number = 2
+Enter the Second Number = 3
+The Sum of 2 and 3 is = 5
+
+*/
