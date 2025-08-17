644181a95030e225fceb2f237a48c47980a8362d
Gayathri Girish <gayathri120906@gmail.com>
2025-08-17 16:03:58 +0530

    Add files via upload

---
+++ 03. Program_to_Input_Name_and_Print_Namah_Shivaya.cpp
@@ -0,0 +1,27 @@
+// Program to take a name as input and print "Namah Shivaya , [name] !"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "Enter your name = ";
+    cin >> name;
+
+    cout << "Namah Shivaya , " << name << "!" << endl;
+
+    return 0;
+}
+
+
+
+/*
+
+Output -
+
+Enter your name = Gayathri
+Namah Shivaya , Gayathri!
+
+*/
