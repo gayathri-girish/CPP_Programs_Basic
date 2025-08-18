668d23415bad87de03d8ae6326adf34f1f44fb97
Gayathri Girish <gayathri120906@gmail.com>
2025-08-18 20:03:54 +0530

    Add files via upload

---
+++ 14. Program_to_Calculate_the_Square_of_Number.cpp
@@ -0,0 +1,30 @@
+// Program to Calculate the square of a number .
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a , square;                                              // square = square of the number .
+
+    cout << "Enter a number = ";
+    cin >> a;
+
+    square = a * a;                                              // Square of a number formula = number * number .
+
+    cout << "Square of " << a << " is " << square << endl;
+
+    return 0;
+}
+
+
+
+/*
+
+// Output -
+
+Enter a number = 3
+Square of 3 is 9
+
+*/
